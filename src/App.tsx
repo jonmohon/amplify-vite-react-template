6abedf6dd7c3ea58a73de26ce965c70e179ba5ad
Jonathan <jonathan@webjoint.com>
2024-11-09 11:04:41 -0600

    removed test

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos test2</h1>
+      <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
