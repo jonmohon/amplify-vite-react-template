93d639252aec5a6e0c43a462d2d95d8e78effee9
Jonathan <jonathan@webjoint.com>
2024-11-09 13:13:37 -0600

    removed errored lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import Sidebar from './components/Sidebar';
 
@@ -10,7 +9,6 @@ function App() {
       <Sidebar /> {/* Sidebar on the left */}
       <main style={{ padding: '20px', flex: 1 }}>
         <h1>Welcome, {user?.username}!</h1>
-        <p>Email: {user?.attributes?.email}</p>
         <button onClick={signOut}>Sign Out</button>
       </main>
     </div>
