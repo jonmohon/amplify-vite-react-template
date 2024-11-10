bc3aba4ac6bc9dd2178ebc2f736813eeb711c410
Jonathan <jonathan@webjoint.com>
2024-11-10 11:46:17 -0600

    changed main color

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -3,8 +3,8 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#0A89FF', // Ensure this is the correct blue you want
-      dark: '#006FCC', // Optional: set a darker blue for hover effects
+      main: '#1B2430', // Ensure this is the correct blue you want
+      dark: '#0A89FF', // Optional: set a darker blue for hover effects
     },
     secondary: {
       main: '#ffffff', // For text, if needed
