927559bad0029a87c5c6ed207f34483d3b568deb
Jonathan <jonathan@webjoint.com>
2024-12-13 10:04:09 -0600

    ?

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,9 @@
+// amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { api } from './api/resource';
 import { data } from './data/resource';
 
 export const backend = defineBackend({
   auth,
-  api,
   data
 });
